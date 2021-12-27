86620434a21d7dafa945a750cbeeddcf02dc1def
WFTurnip <95839021+WFTurnip@users.noreply.github.com>
2021-12-27 21:34:27 +0900

    changed indent & lines

--- reversi.py
+++ reversi.py
@@ -13,11 +13,14 @@ df = df.drop(index=df[df["transcript"].str.contains('transcript')].index)
 transcripts_raw = df["transcript"].str.extractall('(..)')
 
 # Indexを再構成して、1行1手の表にする
-transcripts_df = transcripts_raw.reset_index().rename(columns={"level_0":"tournamentId" , "match":"move_no", 0:"move_str"})
+transcripts_df = transcripts_raw.reset_index().rename(
+    columns={"level_0": "tournamentId", "match": "move_no", 0: "move_str"})
 
 # 列の値を数字に変換するdictonaryを作る
+
+
 def left_build_conv_table():
-    left_table = ["a","b","c","d","e","f","g","h"]
+    left_table = ["a", "b", "c", "d", "e", "f", "g", "h"]
     left_conv_table = {}
     n = 1
 
@@ -26,25 +29,36 @@ def left_build_conv_table():
         n = n + 1
     return left_conv_table
 
+
 left_conv_table = left_build_conv_table()
 
 # dictionaryを使って列の値を数字に変換する
+
+
 def left_convert_colmn_str(col_str):
     return left_conv_table[col_str]
 
 # 1手を数値に変換する
+
+
 def convert_move(v):
-    l = left_convert_colmn_str(v[:1]) # 列の値を変換する
-    r = int(v[1:]) # 行の値を変換する
+    l = left_convert_colmn_str(v[:1])  # 列の値を変換する
+    r = int(v[1:])  # 行の値を変換する
     return np.array([l - 1, r - 1], dtype='int8')
 
-transcripts_df["move"] = transcripts_df.apply(lambda x: convert_move(x["move_str"]), axis=1)
+
+transcripts_df["move"] = transcripts_df.apply(
+    lambda x: convert_move(x["move_str"]), axis=1)
 
 # 盤面の中にあるかどうかを確認する
+
+
 def is_in_board(cur):
     return cur >= 0 and cur <= 7
 
 # ある方向(direction）に対して石を置き、可能なら敵の石を反転させる
+
+
 def put_for_one_move(board_a, board_b, move_row, move_col, direction):
     board_a[move_row][move_col] = 1
 
@@ -57,7 +71,7 @@ def put_for_one_move(board_a, board_b, move_row, move_col, direction):
     cur_col += direction[1]
     reverse_cnt = 0
     while is_in_board(cur_row) and is_in_board(cur_col):
-        if tmp_b[cur_row][cur_col] == 1: # 反転させる
+        if tmp_b[cur_row][cur_col] == 1:  # 反転させる
             tmp_a[cur_row][cur_col] = 1
             tmp_b[cur_row][cur_col] = 0
             cur_row += direction[0]
@@ -69,33 +83,45 @@ def put_for_one_move(board_a, board_b, move_row, move_col, direction):
             return board_a, board_b, reverse_cnt
     return board_a, board_b, reverse_cnt
 
+
 # 方向の定義（配列の要素は←、↖、↑、➚、→、➘、↓、↙に対応している）
-directions = [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
+directions = [[-1, 0], [-1, 1], [0, 1],
+              [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
 
 # ある位置に石を置く。すべての方向に対して可能なら敵の石を反転させる
-def put(board_a, board_b ,move_row, move_col):
+
+
+def put(board_a, board_b, move_row, move_col):
     tmp_a = board_a.copy()
     tmp_b = board_b.copy()
     global directions
     reverse_cnt_amount = 0
     for d in directions:
-        board_a ,board_b, reverse_cnt = put_for_one_move(board_a, board_b, move_row, move_col, d)
+        board_a, board_b, reverse_cnt = put_for_one_move(
+            board_a, board_b, move_row, move_col, d)
     reverse_cnt_amount += reverse_cnt
 
-    return board_a , board_b, reverse_cnt_amount
+    return board_a, board_b, reverse_cnt_amount
 
 # 盤面の位置に石がないことを確認する
+
+
 def is_none_state(board_a, board_b, cur_row, cur_col):
     return board_a[cur_row][cur_col] == 0 and board_b[cur_row][cur_col] == 0
 
-# 盤面に石が置けるかを確認する（ルールでは敵の石を反転できるような位置にしか石を置けない）  
+# 盤面に石が置けるかを確認する（ルールでは敵の石を反転できるような位置にしか石を置けない）
+
+
 def can_put(board_a, board_b, cur_row, cur_col):
     copy_board_a = board_a.copy()
     copy_board_b = board_b.copy()
-    _,  _, reverse_cnt_amount = put(copy_board_a, copy_board_b, cur_row, cur_col)
+    _,  _, reverse_cnt_amount = put(
+        copy_board_a, copy_board_b, cur_row, cur_col)
     return reverse_cnt_amount > 0
 
 # パスする必要のある盤面かを確認する
+
+
 def is_pass(is_black_turn, board_black, board_white):
     if is_black_turn:
         own = board_black
@@ -109,20 +135,25 @@ def is_pass(is_black_turn, board_black, board_white):
                 return False
     return True
 
+
 # 変数の初期化
-b_tournamentId = -1 # トーナメント番号
-board_black = [] # 黒にとっての盤面の状態（１試合保存用）
-board_white = [] # 白にとっての盤面の状態（１試合保存用）
-boards_black = [] # 黒にとっての盤面の状態（全トーナメント保存用）
-boards_white = [] # 白にとっての盤面の状態（全トーナメント保存用）
-moves_black = [] # 黒の打ち手（全トーナメント保存用）
-moves_white = [] # 白の打ち手（全トーナメント保存用）
-is_black_turn = True # True = 黒の番、 False = 白の番
+b_tournamentId = -1  # トーナメント番号
+board_black = []  # 黒にとっての盤面の状態（１試合保存用）
+board_white = []  # 白にとっての盤面の状態（１試合保存用）
+boards_black = []  # 黒にとっての盤面の状態（全トーナメント保存用）
+boards_white = []  # 白にとっての盤面の状態（全トーナメント保存用）
+moves_black = []  # 黒の打ち手（全トーナメント保存用）
+moves_white = []  # 白の打ち手（全トーナメント保存用）
+is_black_turn = True  # True = 黒の番、 False = 白の番
 # ターン（黒の番 or 白の番）を切り変える
+
+
 def switch_turn(is_black_turn):
-    return is_black_turn == False # ターンを切り替え
+    return is_black_turn == False  # ターンを切り替え
 
 # 棋譜のデータを１つ読み、学習用データを作成する関数
+
+
 def process_tournament(df):
     global is_black_turn
     global b_tournamentId
@@ -133,44 +164,49 @@ def process_tournament(df):
     global moves_white
     global moves_black
     if df["tournamentId"] != b_tournamentId:
-    # トーナメントが切り替わったら盤面を初期状態にする
+        # トーナメントが切り替わったら盤面を初期状態にする
         b_tournamentId = df["tournamentId"]
-        board_black = np.zeros(shape=(8,8), dtype='int8')
+        board_black = np.zeros(shape=(8, 8), dtype='int8')
         board_black[3][4] = 1
         board_black[4][3] = 1
-        board_white = np.zeros(shape=(8,8), dtype='int8')
+        board_white = np.zeros(shape=(8, 8), dtype='int8')
         board_white[3][3] = 1
         board_white[4][4] = 1
         is_black_turn = True
     else:
-    # ターンを切り替える
+        # ターンを切り替える
         is_black_turn = switch_turn(is_black_turn)
-    if is_pass(is_black_turn, board_black, board_white): # パスすべき状態か確認する
-        is_black_turn = switch_turn(is_black_turn) #パスすべき状態の場合はターンを切り替える
+    if is_pass(is_black_turn, board_black, board_white):  # パスすべき状態か確認する
+        is_black_turn = switch_turn(is_black_turn)  # パスすべき状態の場合はターンを切り替える
 
-  # 黒の番なら黒の盤面の状態を保存する、白の番なら白の盤面の状態を保存する
+    # 黒の番なら黒の盤面の状態を保存する、白の番なら白の盤面の状態を保存する
     if is_black_turn:
-        boards_black.append(np.array([board_black.copy(), board_white.copy()], dtype='int8'))
+        boards_black.append(
+            np.array([board_black.copy(), board_white.copy()], dtype='int8'))
     else:
-        boards_white.append(np.array([board_white.copy(), board_black.copy()], dtype='int8'))
+        boards_white.append(
+            np.array([board_white.copy(), board_black.copy()], dtype='int8'))
 
-  # 打ち手を取得する
+    # 打ち手を取得する
     move = df["move"]
-    move_one_hot = np.zeros(shape=(8,8), dtype='int8')
+    move_one_hot = np.zeros(shape=(8, 8), dtype='int8')
     move_one_hot[move[1]][move[0]] = 1
 
-  # 黒の番なら自分→敵の配列の並びをを黒→白にして打ち手をセットする。白の番なら白→黒の順にして打ち手をセットする
+    # 黒の番なら自分→敵の配列の並びをを黒→白にして打ち手をセットする。白の番なら白→黒の順にして打ち手をセットする
     if is_black_turn:
         moves_black.append(move_one_hot)
-        board_black, board_white, _ = put(board_black, board_white, move[1], move[0])
+        board_black, board_white, _ = put(
+            board_black, board_white, move[1], move[0])
     else:
         moves_white.append(move_one_hot)
-        board_white, board_black, _ = put(board_white, board_black, move[1], move[0])
+        board_white, board_black, _ = put(
+            board_white, board_black, move[1], move[0])
+
 
 # 棋譜データを学習データに展開する
-transcripts_df.apply(lambda x: process_tournament(x), axis= 1)
+transcripts_df.apply(lambda x: process_tournament(x), axis=1)
 
 x_train = np.concatenate([boards_black, boards_white])
-y_train = np.concatenate([moves_black, moves_white])  
+y_train = np.concatenate([moves_black, moves_white])
 # 教師データは8x8の2次元データになっているので、64要素の1次元データにreshapeする
-y_train_reshape = y_train.reshape(-1,64)
+y_train_reshape = y_train.reshape(-1, 64)
--- templates/othello.py
+++ templates/othello.py
@@ -1,9 +1,11 @@
 import numpy as np
+
+
 class othello:
     def __init__(self):
-        #オセロの初期配置
-        self.list = np.zeros((8,8),dtype = int)
-        #1が黒、2が白、0が空白
+        # オセロの初期配置
+        self.list = np.zeros((8, 8), dtype=int)
+        # 1が黒、2が白、0が空白
         self.list[3][3] = 1
         self.list[4][4] = 1
         self.list[3][4] = 2
@@ -11,95 +13,83 @@ class othello:
         # 1（黒）が先行
         self.player = 1
 
-        #ひっくり返すことができるx座標
+        # ひっくり返すことができるx座標
         self.overturn_x = []
-        #ひっくり返すことができるy座標
+        # ひっくり返すことができるy座標
         self.overturn_y = []
-        #dx[0]とdy[0]、dx[1]とdy[1]が組み合わせになっている
+        # dx[0]とdy[0]、dx[1]とdy[1]が組み合わせになっている
 
-    #オセロの碑が置いてあるマスの座標が来た場合の処理
-    def Check(self,x,y):
-        if self.list[x][y] !=0:
+    # オセロの碑が置いてあるマスの座標が来た場合の処理
+    def Check(self, x, y):
+        if self.list[x][y] != 0:
             print("ここには置けません")
             return False
-    
-    #取得した座標にオセロの石を置いたとき、何個ひっくり返せるかの判定
-    #取得された座標はのself.overturn_x、self.overturn_yに格納される
-    #引数はx座標、y座標、プレイヤーの番号
-    def Reverce(self,x,y,player):
+
+    # 取得した座標にオセロの石を置いたとき、何個ひっくり返せるかの判定
+    # 取得された座標はのself.overturn_x、self.overturn_yに格納される
+    # 引数はx座標、y座標、プレイヤーの番号
+    def Reverce(self, x, y, player):
         self.overturn_x = []
         self.overturn_y = []
-        kensa = [-1,0,1]
+        kensa = [-1, 0, 1]
 
-        #置いたマスの縦、横、ななめのひっくり返せる石の座標の取得
-        #取得された座標はのself.overturn_x、self.overturn_yに格納される
+        # 置いたマスの縦、横、ななめのひっくり返せる石の座標の取得
+        # 取得された座標はのself.overturn_x、self.overturn_yに格納される
         for xx in kensa:
             for yy in kensa:
-                #xxとyyが置いた石の座標と被ったときの処理を飛ばす
-                if xx ==0 and yy == 0:
+                # xxとyyが置いた石の座標と被ったときの処理を飛ばす
+                if xx == 0 and yy == 0:
                     continue
                 tmpx = []
                 tmpy = []
                 takasa = 0
-                #直線の石を調べる
+                # 直線の石を調べる
                 while(True):
                     takasa += 1
-                    #x座標とy座標の石を直線的に探す
+                    # x座標とy座標の石を直線的に探す
                     tyokusen_x = x + (xx * takasa)
                     tyokusen_y = y + (yy * takasa)
-                    #rxとryに石はあるのかの判定と、ひっくり返せるかの判定
-                    if 0 <= tyokusen_x < 8 and 0 <= tyokusen_y <8:
+                    # rxとryに石はあるのかの判定と、ひっくり返せるかの判定
+                    if 0 <= tyokusen_x < 8 and 0 <= tyokusen_y < 8:
                         osero = self.list[int(tyokusen_x)][int(tyokusen_y)]
-                        #自分の石が見つかったとき
+                        # 自分の石が見つかったとき
                         if osero == player:
                             if tmpx != [] and tmpy != []:
-                                #overturn.extend(tmp)
+                                # overturn.extend(tmp)
                                 self.overturn_x.append(tmpx)
                                 self.overturn_y.append(tmpy)
-                        #相手の石が見つかったとき
+                        # 相手の石が見つかったとき
                         elif osero != player:
                             if osero == 0:
                                 break
                             else:
-                            #tmp.append((rx,ry))
+                                # tmp.append((rx,ry))
                                 tmpx.append(tyokusen_x)
                                 tmpy.append(tyokusen_y)
                     else:
                         break
 
-
-        
-
-    
-    #プレイヤーがオセロを置くことができる位置を検出
-    #def Put(self,player):
+    # プレイヤーがオセロを置くことができる位置を検出
+    # def Put(self,player):
         #overturn = []
-        #for x in range(8):
-            #for y in range(8):
-                #石が置いてあるマスの場合
-                #if self.list[y][x] != 0:
-                    #continue
-                #この座標に置いた場合のひっくり返せる石をさがす
-                #self.Reverce(x,y,player)
-                #if  self.overturn_x == [] and self.overturn_y == []:
-                    #continue
-                #else:
-                    #overturn.append((x,y))
-                    #self.overturn_x.append(x)
-                    #self.overturn_y.append(y)
-            #return overturn
-        
-
-
-                
-
-        
-
-
+        # for x in range(8):
+            # for y in range(8):
+                # 石が置いてあるマスの場合
+                # if self.list[y][x] != 0:
+                    # continue
+                # この座標に置いた場合のひっくり返せる石をさがす
+                # self.Reverce(x,y,player)
+                # if  self.overturn_x == [] and self.overturn_y == []:
+                    # continue
+                # else:
+                    # overturn.append((x,y))
+                    # self.overturn_x.append(x)
+                    # self.overturn_y.append(y)
+            # return overturn
 
 
 if __name__ == '__main__':
-    #マスに対応する２次元リストを生成
+    # マスに対応する２次元リストを生成
     #list = np.zeros((8,8),dtype = int)
     #list[3][3] = 1
     #list[4][4] = 1
@@ -107,17 +97,17 @@ if __name__ == '__main__':
     #list[4][3] = 2
     app = othello()
     print(app.list)
-    #print(app.overturn_x)
-    #print(app.overturn_y)
+    # print(app.overturn_x)
+    # print(app.overturn_y)
     print("x座標の入力")
     valx = input()
     print("y座標の入力")
     valy = input()
-    app.Check(int(valx),int(valy))
-    app.Reverce(int(valx),int(valy),app.player)
+    app.Check(int(valx), int(valy))
+    app.Reverce(int(valx), int(valy), app.player)
     print(app.overturn_x)
     print(app.overturn_y)
-    #while(True):
-        #valx = input()
-        #valy = input()
-        #print(list)
+    # while(True):
+    #valx = input()
+    #valy = input()
+    # print(list)

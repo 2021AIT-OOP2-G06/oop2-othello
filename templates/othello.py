96863cc120ff647b70f2d3f7745497ae183fbb03
kkisan60 <1701kisan@gmail.com>
2021-12-30 12:27:05 +0900

    オセロ更新
    一通り動くようにした

--- templates/othello.py
+++ templates/othello.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.core.fromnumeric import size
 class othello:
     def __init__(self):
         #オセロの初期配置
@@ -19,19 +20,23 @@ class othello:
         #Putで検出した座標
         self.put_x = []
         self.put_y = []
+        #連続したパスの回数、2でゲームセット
+        self.pa=0
 
-    #オセロの碑が置いてあるマスの座標が来た場合の処理
+    #オセロの石が置いてあるマスの座標が来た場合の処理
     def Check(self,x,y):
-        if self.list[x][y] !=0:
+        #すでに石が置いてある時
+        if self.list[y][x] !=0:
             print("ここには置けません")
-            return False
+            return True
+        #来た座標に置いた時、石をひっくり返せるかどうか
         for i in range(len(self.put_x)):
             if self.put_x[i] == x and self.put_y[i] == y:
                 print("ここにおけます")
-                self.list[x][y] = self.player
-                return True
+                self.list[y][x] = self.player
+                return False
         print("ここに置けません")
-        return False
+        return True
 
     
     #取得した座標にオセロの石を置いたとき、何個ひっくり返せるかの判定
@@ -60,13 +65,16 @@ class othello:
                     tyokusen_y = y + (yy * takasa)
                     #rxとryに石はあるのかの判定と、ひっくり返せるかの判定
                     if 0 <= tyokusen_x < 8 and 0 <= tyokusen_y <8:
-                        osero = self.list[int(tyokusen_x)][int(tyokusen_y)]
+                        osero = self.list[int(tyokusen_y)][int(tyokusen_x)]
                         #自分の石が見つかったとき
                         if osero == self.player:
                             if tmpx != [] and tmpy != []:
                                 #print("追加しました")
                                 self.overturn_x.append(tmpx)
                                 self.overturn_y.append(tmpy)
+                                break
+                            else:
+                                break
                         #相手の石が見つかったとき
                         elif osero != self.player:
                             if osero == 0:
@@ -87,14 +95,11 @@ class othello:
     def Put(self):
         self.put_x = []
         self.put_y = []
-        #パスした回数 2回連続でゲームセット
-        pa = 0
-        #print(self.list)
         for x in range(8):
             for y in range(8):
                 #石が置いてあるマスの場合
                 #print(self.list[x][y])
-                if self.list[int(x)][int(y)] != 0:
+                if self.list[int(y)][int(x)] != 0:
                     continue
                 #この座標に置いた場合のひっくり返せる石をさがす
                 #print(x,y)
@@ -107,37 +112,56 @@ class othello:
 
     #石をひっくり返す動作
     def Overturn (self,x,y):
+        #ひっくり返せる座標を取得
         self.Reverce(x,y)
-        #print(self.overturn_x[0])
-        #print(self.overturn_y[0])
-        #for i in range(len(self.overturn_x)):
-            #xx =self.overturn_x[0]
-            #yy = self.overturn_y[0]
-            #print(xx,yy)
-            #print(len(self.list))
-            #self.list[xx][yy] = player
-            #print(i)
-        xx = self.overturn_x[0]
-        yy = self.overturn_y[0]
-        print(type(xx))
-        print(xx,yy)
-        print(x)
-        #print(self.list[xx][yy])
-        #self.list[3][4] = self.player
-        self.list[xx][yy] = self.player
-        #if player == 1:
-            #player = 2
-        #else :
-            #player = 1
+        #x座標とy座標を格納
+        xx =self.overturn_x
+        yy = self.overturn_y
+        #石をひっくり返す
+        for i in range(len(self.overturn_y)):
+            for j in range(len(self.overturn_y[i])):
+                #print(self.overturn_x[i][j])
+                #print(self.overturn_y[i][j])
+                self.list[yy[i][j]][xx[i][j]] = self.player
+        #プレイヤーの交代
+        if self.player == 1:
+            self.player = 2
+        else :
+            self.player = 1
 
     #パスをする場面かどうか
     def Pass(self):
-        pa =0
-        if self.overturn_x == []:
-            pa += 1
+        #print("pass発動")
+        if self.put_x == []:
+            self.pa += 1
+            if self.pa == 1:
+                print(f"{app.player}はパスです")
+            if self.player == 1:
+                self.player = 2
+            else :
+                self.player = 1
         else :
-            pa = 0
+            self.pa = 0
     #勝ち負けの判定
+    def WinLose(self):
+        black =0
+        white = 0
+        for x in range(8):
+            for y in range(8):
+                if self.list[y][x] == 1:
+                    black +=1
+                elif self.list[y][x] == 2:
+                    white += 1
+
+        if black > white:
+            #print("黒の勝ちです")
+            print("黒の勝ちです")
+        elif white > black:
+            print("白の勝ちです")
+        else:
+            print("同点です")
+
+            
 
         
 
@@ -150,30 +174,37 @@ class othello:
 
 
 if __name__ == '__main__':
-    #マスに対応する２次元リストを生成
-    #list = np.zeros((8,8),dtype = int)
-    #list[3][3] = 1
-    #list[4][4] = 1
-    #list[3][4] = 2
-    #list[4][3] = 2
     app = othello()
-    print(app.list)
-    app.Put()
-    print(app.put_x)
-    print(app.put_y)
-    #print(app.overturn_y)
-    print("x座標の入力")
-    valx = input()
-    print("y座標の入力")
-    valy = input()
-    app.Check(int(valx),int(valy))
-    app.Overturn(int(valx),int(valy))
-    print(app.list)
-    #app.Reverce(int(valx),int(valy),app.player)
-    #print(app.overturn_x)
-    #print(app.overturn_y)
+    while(True):
+        print(app.list)
+        print(app.player)
+        #app.playerが石を置ける座標を検出する
+        app.Put()
+        print(app.put_x)
+        print(app.put_y)
 
-    #while(True):
-        #valx = input()
-        #valy = input()
-        #print(list)
+        #パスかどうかの判断
+        app.Pass()
+        #パス
+        if app.pa ==1:
+            continue
+        #ゲーム終了
+        elif app.pa == 2:
+            print("ゲーム終了です")
+            app.WinLose()
+            break
+
+        print("x座標の入力")
+        valx = input()
+        print("y座標の入力")
+        valy = input()
+
+        #座標の位置に石を置けるかの判断
+        if valx == "" or valy == "":
+            continue
+        check =app.Check(int(valx),int(valy))
+        if check:
+            continue
+
+        #石をひっくり返す
+        app.Overturn(int(valx),int(valy))

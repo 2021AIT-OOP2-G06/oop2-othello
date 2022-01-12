56b409d6b4f947b36f218aedbf3d894f2559b3b8
MjimaSmichi <k20113kk@aitech.ac.jp>
2022-01-12 14:22:49 +0900

    一応保存しておく

---
+++ static/index.js
@@ -0,0 +1,21 @@
+$("#tbl1 td").bind('click', function(){
+    $tag_td = $(this)[0];
+    $tag_tr = $(this).parent()[0];
+    console.log("%s, %s", $tag_tr.rowIndex, $tag_td.cellIndex);
+    data={"lows":Number($tag_tr.rowIndex),"cols":Number($tag_td.cellIndex)}
+    fetch('/url', {method: 'POST', body: data,})
+    .then(function (response) {
+        response.json().then((data) => {
+
+            /*if (data.error) {
+                // エラーの受信
+                document.getElementById('notice').innerHTML = data.error
+            }*/
+
+            if (data.result) {
+                // メッセージの受信
+
+            }
+        })
+    })
+});

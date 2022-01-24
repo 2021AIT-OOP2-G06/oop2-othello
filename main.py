cd4e738c99cdcba5091381032e730640fef03034
松田　雄飛 <k20109kk@aitech.ac.jp>
2022-01-25 03:34:56 +0900

    add:randomcpu

--- main.py
+++ main.py
@@ -13,6 +13,25 @@ def index():
     score = 'Score'
     return render_template('index.html', title= title, score = score)
 
+@app.route('/getPosition', methods=['POST'])
+def getPosition():
+    
+    row = request.json['row']
+    col = request.json['col']
+    player = request.json['player']
+    sms = othello.selectCell(row, col,player)
+    print(sms)
+    return jsonify( grid=othello.grid, message=sms)
+
+@app.route('/start', methods=['POST'])
+def start():
+    return jsonify( grid=othello.grid)
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    othello.grid = request.json['respaldo']
+    return jsonify( grid=othello.grid)
+
 @app.route('/')
 def top():
     return render_template('toppage.html')
@@ -21,21 +40,22 @@ def top():
 def cpu_battle():
     return render_template('cpu.html')
 
-@app.route('/getPosition', methods=['POST'])
-def getPosition():
+@app.route('/cpugetPosition', methods=['POST'])
+def cpugetPosition():
+    
     row = request.json['row']
     col = request.json['col']
     player = request.json['player']
-    sms = cpu.selectCell(row, col, 1)
+    sms = cpu.selectCell(row, col,player)
     print(sms)
     return jsonify( grid=cpu.grid, message=sms)
 
-@app.route('/start', methods=['POST'])
-def start():
+@app.route('/cpustart', methods=['POST'])
+def cpustart():
     return jsonify( grid=cpu.grid)
 
-@app.route('/reset', methods=['POST'])
-def reset():
+@app.route('/cpureset', methods=['POST'])
+def cpureset():
     cpu.grid = request.json['respaldo']
     return jsonify( grid=cpu.grid)
 

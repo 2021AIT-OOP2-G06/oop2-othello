af548dc5782bc4f894f44014d8979412069ce7ab
松田　雄飛 <k20109kk@aitech.ac.jp>
2022-01-24 17:47:35 +0900

    fix:不要箇所削除

--- random-cpu.py
+++ random-cpu.py
@@ -48,6 +48,192 @@ def selectCell(row,col,player):
         return "PASS"
     else :
         return "OK"
+    
+
+    
+
+#redirecciona a los posibles movimientos
+def isValid(row,col,player):
+        horizontal(row,col,player)
+        vertical(row, col, player)
+        diagonal(row, col, player)
+
+def horizontal(row,col,player):
+        lenG = lenGrid()
+        if(row<=lenG-1 and col<=lenG-1):
+            if(player==1):
+                if (col+1 <= lenG-1):
+                    if(grid[row][col+1]==2):
+                        grid[row][col+1] = 1
+                        horizontal(row, col+1, player)
+                else:
+                    if (grid[row][col] == 2):
+                        grid[row][col] = 1
+                        horizontal(row, col, player)
+
+            if (player == 2):
+                if (col+1 <= lenG-1):
+                    if (grid[row][col+1] == 1):
+                        grid[row][col+1] = 2
+                        horizontal(row, col+1, player)
+                else:
+                    if (grid[row][col] == 1):
+                        grid[row][col] = 2
+                        horizontal(row,col, player)
+
+        if(row>=0 and col>=0):
+            if(player==1):
+                if (col-1 >= 0):
+                    if(grid[row][col-1]==2):
+                        grid[row][col-1] = 1
+                        horizontal(row, col-1, player)
+                else:
+                    if (grid[row][col] == 2):
+                        grid[row][col] = 1
+                        horizontal(row, col, player)
+            if (player == 2):
+                if(col-1 >= 0):
+                    if (grid[row][col-1] == 1):
+                        grid[row][col-1] = 2
+                        horizontal(row, col-1, player)
+                else:
+                    if (grid[row][col] == 1):
+                        grid[row][col] = 2
+                        horizontal(row,col, player)
+
+def vertical(row,col,player):
+    #垂直
+    lenG=lenGrid()
+    if(row>= 0 and col>=0 ):
+        up=grid[row-1][col]
+        if (player == 1):
+            if (up == 2):
+                grid[row-1][col] = 1
+                vertical(row-1,col, player)
+        if (player == 2):
+            if (up == 1):
+                grid[row-1][col] = 2
+                vertical(row-1, col, player)
+    if(row <= lenG-1 and col <= lenG-1):
+        if(row+1 <= lenG-1):
+            dow=grid[row+1][col]
+            if (player == 1):
+                if (dow == 2):
+                    grid[row+1][col] = 1
+                    vertical(row+1,col, player)
+            if (player == 2):
+                if (dow == 1):
+                    grid[row+1][col] = 2
+                    vertical(row+1,col, player)
+        else:
+            actual = grid[row][col]
+            if (player == 1):
+                    if (actual == 2):
+                        grid[row][col] = 1
+                        vertical(row, col, player)
+            if (player == 2):
+                if (actual == 1):
+                    grid[row][col] = 2
+                    vertical(row, col, player)
+
+def diagonal(row,col,player):
+    lenG = lenGrid()
+    if (player == 1):
+        if (row >= 0 and col >= 0):
+            if(row-1 >= 0 and col+1 <= lenG-1):
+                    upDer = grid[row - 1][col + 1]
+                    if (upDer  == 2):
+                        grid[row-1][col+1] = 1
+                        diagonal(row-1,col+1,player)
+        if (row >= 0 and col >= 0):
+            if (row-1>= 0 and col-1>=0):
+                upIzq = grid[row-1][col-1]
+                if (upIzq == 2):
+                    grid[row-1][col-1] = 1
+                    diagonal(row-1,col-1,player)
+        if (row <= lenG - 1 and col <= lenG - 1):
+            if (row+1<= lenG-1 and col+1 <= lenG-1):
+                dowDer = grid[row+1][col+1]
+                if (dowDer == 2):
+                    grid[row+1][col+1] = 1
+                    diagonal(row+1,col+1,player)
+        if (row <= lenG - 1 and col <= lenG - 1):
+            if (row + 1 <= lenG-1 and col - 1 >= 0):
+                dowIzq = grid[row + 1][col - 1]
+                if (dowIzq == 2):
+                    grid[row+1][col-1] = 1
+                    diagonal(row+1,col-1,player)
+
+    if (player == 2):
+        if (row >= 0 and col >= 0):
+            if(row-1 >= 0 and col+1 <= lenG-1):
+                    upDer = grid[row - 1][col + 1]
+                    if (upDer  == 1):
+                        grid[row-1][col+1] = 2
+                        diagonal(row-1,col+1,player)
+        if (row >= 0 and col >= 0):
+            if (row-1>= 0 and col-1>=0):
+                upIzq = grid[row-1][col-1]
+                if (upIzq == 1):
+                    grid[row-1][col-1] = 2
+                    diagonal(row-1,col-1,player)
+        if (row <= lenG - 1 and col <= lenG - 1):
+            if (row+1<= lenG-1 and col+1 <= lenG-1):
+                dowDer = grid[row+1][col+1]
+                if (dowDer == 1):
+                    grid[row+1][col+1] = 2
+                    diagonal(row+1,col+1,player)
+
+        if (row <= lenG - 1 and col <= lenG - 1):
+            if (row + 1 <= lenG-1 and col - 1 >= 0):
+                dowIzq = grid[row + 1][col - 1]
+                if (dowIzq == 1):
+                    grid[row+1][col-1] = 2
+                    diagonal(row+1,col-1,player)
+
+def invalido(row,col):
+    #無効
+    lenG=lenGrid()
+    if (row >= 0):
+        if(row-1 >= 0):
+            if(grid[row-1][col]!=0 ):
+                return True
+    if(row <= lenG-1):
+        if(row+1 <= lenG-1):
+            if(grid[row+1][col]):
+                return True
+    if(col <= lenG-1):
+        if(col+1 <= lenG-1):
+            if(grid[row][col+1] != 0):
+                return True
+    if(col >= 0):
+        if(col-1 >= 0):
+            if(grid[row][col-1] != 0):
+                return True
+    if(row >= 0 and col >= 0):
+        if(row-1 >= 0 and col-1 >= 0):
+            if(grid[row-1][col-1] != 0):
+                return True
+    if (row <= lenG-1 and col  <= lenG-1):
+        if (row + 1 <= lenG-1 and col + 1 <= lenG-1):
+            if (grid[row + 1][col + 1] != 0):
+                return True
+    if (row >= 0 and col <= lenG - 1):
+        if (row - 1 >=  0 and col + 1 <= lenG - 1):
+            if (grid[row - 1][col + 1] != 0):
+                return True
+
+    if (row <= lenG-1 and col >= 0):
+        if (row + 1 <= lenG-1 and col - 1 >= 0):
+            if (grid[row + 1][col - 1] != 0):
+                return True
+
+def lenGrid():
+    cont=0
+    for x in grid:
+        cont+=1
+    return cont
+
 
     # 石をひっくり返す動作
 def Overturn(x,y,player):
@@ -97,6 +283,7 @@ def Overturn(x,y,player):
         # x座標とy座標を格納
     xx = overturn_x
     yy = overturn_y
+    print(xx,yy)
         # 石をひっくり返す
     for i in range(len(overturn_y)):
         for j in range(len(overturn_y[i])):
